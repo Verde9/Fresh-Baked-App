d42e407db642edc063e3e8b937685d3eb3f94123
JoeyHJava <3studyemail@gmail.com>
2022-06-14 14:52:03 -0700

    create pizza code

---
+++ client/createPizzaScript.js
@@ -0,0 +1,85 @@
+let count = 1;
+let pizza = {
+  ingrediants: [],
+};
+let displayCount = document.getElementById("count");
+let subtract = document
+  .getElementById("subtract")
+  .addEventListener("click", handleSubtract);
+
+document.getElementById("add").addEventListener("click", handleAdd);
+
+function handleSubtract() {
+  if (count < 1) return;
+  count = count -= 1;
+  handleCount();
+}
+
+function handleAdd() {
+  count = count += 1;
+  handleCount();
+}
+
+function handleCount() {
+  displayCount.innerText = count;
+  pizza.count = count;
+}
+
+let pizzBtn = document.querySelectorAll("input[name='pizzaSize']");
+
+let selected = "";
+let findSelected = () => {
+  selected = document.querySelector("input[name=pizzaSize]:checked").value;
+  pizza.size = selected;
+};
+
+pizzBtn.forEach((radioBtn) => {
+  radioBtn.addEventListener("change", findSelected);
+});
+//******************************************************************************************** */
+let crustBtn = document.querySelectorAll("input[name='pizzaCrust']");
+
+let selectedCrust = "";
+let findSelectedCrust = () => {
+  selected = document.querySelector("input[name=pizzaCrust]:checked").value;
+  pizza.crust = selected;
+};
+
+crustBtn.forEach((radioBtn) => {
+  radioBtn.addEventListener("change", findSelectedCrust);
+});
+//********************************************************************************************
+
+let sauceBtn = document.querySelectorAll("input[name='pizzaSauce']");
+
+let selectedSauce = "";
+let findSelectedSauce = () => {
+  selected = document.querySelector("input[name=pizzaSauce]:checked").value;
+  pizza.sauce = selected;
+};
+
+sauceBtn.forEach((radioBtn) => {
+  radioBtn.addEventListener("change", findSelectedSauce);
+});
+// findSelected()
+//********************************************************************************************
+
+var ingElement = document.getElementById("ingediants");
+var checkBoxes = ingElement.querySelectorAll('input[type="checkbox"]');
+document.getElementById("cartButton").addEventListener("click", getData);
+
+let result = [];
+
+function getData() {
+  
+  result = [];
+  checkBoxes.forEach((item) => {
+    if (item.checked) {
+      let data = {
+        item: item.value,
+      };
+      pizza.ingrediants.push(data)
+    }
+  });
+}
+
