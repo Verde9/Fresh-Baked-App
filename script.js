e9be6dd7293eca648a76f4480028505b2993a42f
Verde9 <gcg145@gmail.com>
2022-06-14 10:08:55 -0700

    Added index.html script.js & style.css

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("pizza_creation_button").addEventListener("click", function (event) {
+    event.preventDefault();
+    createPizza();
+});
+
+
+function createPizza() {
+    console.log("Creating pizza");
+}
