b82e0245b62a13c2d4137d79b5dc754be4010ad0
Andy Vargas <94484667+andyvn1@users.noreply.github.com>
2022-06-15 10:54:50 -0700

    Tracker and Check out page

---
+++ checkout.js
@@ -0,0 +1,28 @@
+//checkoutjs
+//order number with its function
+
+const order = Math.floor(Math.random() * 10000000);
+
+const orderNumberPara = document.getElementById("on");
+orderNumberPara.innerText = "Order: " + order;
+// orderNumberPara.innerHTML = "Order: " + order;
+
+//editOrder BTN with itts fuction
+const editButton = document.getElementById("editOrder_btn");
+editButton.addEventListener("click", editOrder);
+
+const goBack = document.getElementById("goBack_btn");
+goBack.addEventListener("click", editOrder);
+
+const orderBtn = document.getElementById("order_btn");
+orderBtn.addEventListener("click", orderPage);
+
+function editOrder() {
+  //window.location.href = /*"///index.html for pizza order selection///"*/
+  console.log(a);
+}
+
+function orderPage() {
+  //window.location.href = /*"///index.html for tracking page and save all user inputs///"*/
+  console.log(a);
+}
