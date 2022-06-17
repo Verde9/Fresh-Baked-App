61258afcdaa9d36dd0e8324b9b14f151aacd79f0
Andy Vargas <94484667+andyvn1@users.noreply.github.com>
2022-06-16 17:04:12 -0700

    Change button and link the go back and order button to the page

--- checkout.js
+++ checkout.js
@@ -19,15 +19,13 @@ function getOrder() {
     });
 }
 console.log("pizzaObj", pizzaObj);
-
-const order = Math.floor(Math.random() * 10000000);
+//No longer need it since we bringing order number from the server
+//const order = Math.floor(Math.random() * 10000000);
 //storing the Html p tag into a variable using its ID
 const orderNumberPara = document.getElementById("on");
+
 //inner text into the p tag wich it would dynamically display order number
-orderNumberPara.innerText = "Order: " + order;
-//editOrder BTN calling its fuction
-const editButton = document.getElementById("editOrder_btn");
-editButton.addEventListener("click", editOrder);
+
 //go back button calling its function
 const goBack = document.getElementById("goBack_btn");
 goBack.addEventListener("click", function (e) {
@@ -40,13 +38,8 @@ goBack.addEventListener("click", function (e) {
 
 const orderBtn = document.getElementById("order_btn");
 orderBtn.addEventListener("click", orderPage);
-//function utilize for edit and go back"it will take yout to the previus page"
-function editOrder() {
-  //window.location.href = /*"///index.html for pizza order selection///"*/
-  console.log(order);
-}
+
 //function utilize for order"it will take yout to the next page"
 function orderPage() {
-  //window.location.href = /*"///index.html for tracking page and save all user inputs///"*/
-  console.log(order);
+  window.location.href = "./tracker.html";
 }
